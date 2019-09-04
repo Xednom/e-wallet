0dc7f0018cc75293139784b709579a9a59468b07
Monde <monde.lacanlalay@gmail.com>
2019-09-05 04:07:27 +0800

    added a def for user to return a full name in api serializer

--- wallet/api/serializers.py
+++ wallet/api/serializers.py
@@ -5,10 +5,15 @@ from wallet.models import Cash
 class CashSerializer(serializers.ModelSerializer):
     date = serializers.SerializerMethodField()
     card_details = serializers.StringRelatedField(read_only=True)
+    user = serializers.SerializerMethodField()
 
     class Meta:
         model = Cash
         fields = "__all__"
+
+    def get_user(self, instance):
+        request = self.context.get("request")
+        return '%s %s' % (request.user.first_name, request.user.last_name)
 
     def get_date(self, instance):
         return instance.date.strftime("%B %d, %Y")
