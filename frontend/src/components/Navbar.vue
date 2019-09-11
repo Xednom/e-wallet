59eb7b0c58e26f13bca0bc527adb4c5b79d2ceaf
Monde <monde.lacanlalay@gmail.com>
2019-09-11 14:58:38 +0800

    added the navbar

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <router-link class="navbar-brand"
+      :to="{ name: 'home' }">E-Wallet</router-link>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <router-link class="nav-link"
+            :to="{ name: 'home' }">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link"
+            :to="{ name: 'about' }">About</router-link>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdownMenuLink"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >Dropdown link</a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a href="#">Profile</a>
+              <hr>
+              <a class="dropdown-item" href="/accounts/logout/">Logout</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavbarComponent"
+};
+</script>
+
+<style>
+.my-navbar {
+  border-bottom: 1px solid #ddd;
+}
+.navbar-brand {
+  font-weight: bold;
+  font-size: 130%;
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <h1>Home page</h1>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+  
 };
 </script>
+
+<style>
+
+</style>
