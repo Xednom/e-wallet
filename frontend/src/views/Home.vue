977f0b79be690d47e5152609eaff2eed7066b58b
Monde <monde.lacanlalay@gmail.com>
2019-09-11 19:14:47 +0800

    added the card for total amount, added and deducted in the home page.

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,33 @@
 <template>
   <div class="home">
     <div class="container">
+      <div class="row">
+        <div class="col-md-4 col-sm-12">
+          <div class="card border-success mb-3 cash-in" style="max-width: 18rem;">
+            <div class="card-header bg-transparent border-success">Total Amount Added</div>
+            <div class="card-body text-success">
+              <p class="card-text">&#8369;{{ totalAmountAdded }}</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-12">
+          <div class="card border-warning mb-3 cash-in" style="max-width: 18rem;">
+            <div class="card-header bg-transparent border-warning">Total Amount Deducated</div>
+            <div class="card-body text-warning">
+              <p class="card-text">&#8369;{{ totalAmountDeducted }}</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-12">
+          <div class="card border-info mb-3 cash-in" style="max-width: 18rem;">
+            <div class="card-header bg-transparent border-info">Total Amount</div>
+            <div class="card-body text-info">
+              <p v-for="cash in cashes" :key="cash"
+              class="card-text">&#8369;{{ Number(cash.total_amount).toLocaleString() }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="card wallet-transactions">
         <h5 class="card-header">Wallet Transactions</h5>
         <div class="card-body">
@@ -32,27 +59,39 @@
 
 <script>
 // @ is an alias to /src
-import { apiService } from "@/common/api.service.js"
+import { apiService } from "@/common/api.service.js";
 export default {
   name: "home",
   data() {
     return {
       cashes: [],
       loading: false
+    };
+  },
+  computed: {
+    totalAmountAdded() {
+      return this.cashes.reduce(function(sum, cashes) {
+        return sum + parseFloat(cashes.amount_added);
+      }, 0);
+    },
+    totalAmountDeducted() {
+      return this.cashes.reduce(function(sum, cashes) {
+        return sum + parseFloat(cashes.amount_deducted);
+      }, 0);
     }
   },
   methods: {
     getTransaction() {
       this.loading = true;
-      let endpoint = `/api/v1/cash/`
+      let endpoint = `/api/v1/cash/`;
       apiService(endpoint)
-      .then(data => {
-        this.cashes.push(...data.results);
-        this.loading = false;
-      })
-      .catch(err => {
-        console.log(err);
-      })
+        .then(data => {
+          this.cashes.push(...data.results);
+          this.loading = false;
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
   },
   created() {
@@ -61,7 +100,8 @@ export default {
 };
 </script>
 <style>
-.wallet-transactions {
+.wallet-transactions,
+.cash-in {
   margin: 20px 0 0 0;
 }
 </style>
